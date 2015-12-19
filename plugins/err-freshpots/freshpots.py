18b521dc6240d6734833c85dcc0daba5a9e19792
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2015-12-19 12:22:32 +0200

    FreshPots implementation
    Plugin kinda works now, the config system is somehow incomprehensible to
    me still but now it does its thang.

--- plugins/err-freshpots/freshpots.py
+++ plugins/err-freshpots/freshpots.py
@@ -1,15 +1,24 @@
 """A plugin for telling people when there is coffee."""
 
-from errbot import BotPlugin, botcmd, webhook
-import explorerhat
 import logging
+import re
 
-LOG = logging.getLogger('freshpots')
+from errbot import BotPlugin, botcmd, webhook
+import explorerhat  # pylint: disable=import-error
 
-class FreshPots(BotPlugin):
+LOG = logging.getLogger('errbot.plugin.FreshPots')
+
+
+class FreshPots(BotPlugin):  # pylint: disable=too-many-ancestors
     """Fresh pots plugin class"""
-    min_err_version = '3.2.0' # Optional, but recommended
-    max_err_version = '4.0.0' # Optional, but recommended
+    min_err_version = '3.2.0'  # Optional, but recommended
+    max_err_version = '4.0.0'  # Optional, but recommended
+
+    def __init__(self, bot):
+        super(FreshPots, self).__init__(bot)
+        self.bot_nick = bot.bot_config.CHATROOM_FN
+        self.channels = []
+        LOG.debug("Expecting nick to be %s", self.bot_nick)
 
     def activate(self):
         """Triggers on plugin activation
@@ -17,23 +26,42 @@ class FreshPots(BotPlugin):
         You should delete it if you're not using it to override any default behaviour"""
         super(FreshPots, self).activate()
 
+        if 'channels' in self:
+            self.channels = self['channels']
+
+        self.configure(self.get_configuration_template())
+        LOG.info("konffis on %s", self.config)
+
+        # start listening on a button
+        explorerhat.touch.one.pressed(self.fresh_pots)
+
+    def fresh_pots(self, _iochannel, _event):
+        """Triggered by the hardware, send message."""
+        LOG.info("Would send %s", self.config['image_url'])
+        LOG.info("Channels is %s", self.channels)
+        for dst in self.channels:
+            self.send(dst, self.config['image_url'], message_type='groupchat')
+
     def deactivate(self):
         """Triggers on plugin deactivation
 
         You should delete it if you're not using it to override any default behaviour"""
+        self['channels'] = self.channels
         super(FreshPots, self).deactivate()
 
     def get_configuration_template(self):
         """Defines the configuration structure this plugin supports
 
         You should delete it if your plugin doesn't use any configuration like this"""
-        return {'IMAGE_URL': 'https://i.imgur.com/510LaLV.gif'}
+        return {
+            'image_url': 'https://i.imgur.com/510LaLV.gifv'
+        }
 
     def check_configuration(self, configuration):
         """Triggers when the configuration is checked, shortly before activation
 
         You should delete it if you're not using it to override any default behaviour"""
-        super(FreshPots, self).check_configuration()
+        super(FreshPots, self).check_configuration(configuration)
 
     def callback_connect(self):
         """Triggers when bot is connected
@@ -41,30 +69,32 @@ class FreshPots(BotPlugin):
         You should delete it if you're not using it to override any default behaviour"""
         pass
 
+    def bot_msg_regex(self, message):
+        """Construct a regex for matching a bot message.
+
+        Args:
+            message (str): regex fragment matching message body.
+
+        Returns:
+            regex that matches bot nick followed by message body.
+        """
+        return r'@?%s[:,]?\s*%s' % (self.bot_nick, message)
+
     def callback_message(self, message):
         """Triggered for every received message that isn't coming from the bot itself
 
         You should delete it if you're not using it to override any default behaviour"""
-        LOG.debug(message)
+        # frm: sender userid
+        # to: channel id
+        # body: text incl nick
+        if message.type == 'groupchat':
+            if re.match(self.bot_msg_regex(r"(ilmoita|kerro|sano)\s+(täällä|tänne|tähän)"), message.body):
+                self.channels.append(message.to)
+                self['channels'] = self.channels
+                self.send(message.to, "ok, alan ilmoittaa täällä",
+                          in_reply_to=message, message_type='groupchat',
+                          groupchat_nick_reply=True)
 
-    def callback_botmessage(self, message):
-        """Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour"""
-        pass
-
-    @webhook
-    def example_webhook(self, incoming_request):
-        """A webhook which simply returns 'Example'"""
-        return "Example"
-
-    # Passing split_args_with=None will cause arguments to be split on any kind
-    # of whitespace, just like Python's split() does
-    @botcmd(split_args_with=None)
-    def example(self, mess, args):
-        """A command which simply returns 'Example'"""
-        return "Example"
-
-    @botcmd(split_args_with=None)
-    def holler(self, msg, args):
-        self.send(ID_HERE, "blarr blarr", message_type="groupchat")
+    @botcmd
+    def debugsay(self, msg, args):
+        self.fresh_pots(None, None)
